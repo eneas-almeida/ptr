49e33f7a840e9f5702e94ea50b4e87ed89720f7b
eneas <eneas.eng@yahoo.com>
2025-12-04 21:58:38 -0300

    feat: inicia a aplicação

---
+++ src/test/java/org/ptr/PtrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ptr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
